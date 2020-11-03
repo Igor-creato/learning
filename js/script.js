b7865272f8a061404fb0ff7dcb8777e567a551b3
Igor <learning@gmail.com>
2020-11-03 16:41:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+" use strict ";
+let number = 4.5;
+
+console.log(number);
+console.log('string' * 9);
+
+const persone = 'Igor';
+const bool = true;
+
+let scheme;
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarrid: false
+};
+
+console.log(obj.name); // Выводит знчение положенное в name (John)
+//console.log(obj ['name']); // тоже самое, но лучше через точку
+
+let arr = ['img.png', 'img2.png', 8, 'apple.jpg', {}, []];
+
+console.log(arr[1]);
+
+// const result = confirm('Are you here?'); 
+// console.log(result); //Есло ок то возвращает true иначе false
+
+// const answer = prompt('Вам есть 18?', ''); //пустые кавычки для ввода ответа
+// console.log(answer); //В переменную answer попадает введенный ответ в виде строки
+
+const answers = [];
+// answers[0] = prompt('Your name?', '');
+// answers[1] = prompt('Your sername?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+// console.log(answers);
+ const category = 'toys';
+
+//  console.log('https://mysite.com/' + category + '/');
+
+//  console.log(`https://mysite.com/${category}/`);
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    ++incr;
+    --decr;
+
+    // console.log(2*4 === 8);
+
+    // && оператор и
+    // || оператор или
+    
+    const isChecked = true,
+          isClose = false;
+
+        //   console.log(isChecked || isClose);
+//Если хотябы один true то в итоге будет true
+
+// ! оператор отрицания != не равно
