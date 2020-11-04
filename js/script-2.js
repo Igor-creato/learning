a46eb059bba31bd46e7f40ad64e4e469bcc6135e
Igor <learning@gmail.com>
2020-11-04 18:25:35 +0300

    For and if practic

--- js/script-2.js
+++ js/script-2.js
@@ -1,14 +1,14 @@
 " use strict ";
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// let personalMovieDB = {
-// count: numberOfFilms,
-// movies: {},
-// actors: {},
-// genres: [],
-// privat: false
-// };
+let personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
 
 
 
@@ -21,3 +21,54 @@
 //      personalMovieDB.movies[c] = d;
 
  
+// for (let i=0; i<2; i++){
+//     let a = prompt('Один из просмотренных фильмов', ''),
+//         b = prompt('На сколько оцените его', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+
+//     } else {
+
+//         i--;
+//     }
+// }
+
+let i=0;
+// while(i<2){
+//         let a = prompt('Один из просмотренных фильмов', ''),
+//             b = prompt('На сколько оцените его', '');
+
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//             i++;
+//     } else {
+
+//         i--;
+//     }
+// }
+
+do{
+        let a = prompt('Один из просмотренных фильмов', ''),
+            b = prompt('На сколько оцените его', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            i++;
+    } else {
+
+        i--;
+    }
+}
+while(i<2);
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотренно мало фильмов');
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log('Классический зритель');
+}else if(personalMovieDB.count > 30){
+    console.log('Киноман');
+}else {
+    console.log('Error');
+}
+console.log(personalMovieDB);
--- js/script.js
+++ js/script.js
@@ -98,11 +98,11 @@ let num = 50;
 // }
 // while (num <55);
 
-for (let i = 1; i < 8; i++){
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
+// for (let i = 1; i < 8; i++){
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
 
-}
+// }
