395ef1fe99dc975cf1a0c93e34fe064a2e89cf61
Igor <learning@gmail.com>
2020-11-05 17:38:33 +0300

    Popup

--- js/myscript.js
+++ js/myscript.js
@@ -9,3 +9,37 @@ setInterval(function start() {
     
 },2000);
 
+window.onload = function () {
+    let popUp,
+        slise,
+        close;
+    document.querySelector('.my-button').onclick = function popUpWindow() {
+        popUp = document.querySelector('.my-popup');
+        slise = document.querySelector('.my-popup-style');
+        // slise.style.cssText = `
+        // margin: 0;
+        // `
+        popUp.style.cssText =`
+        visibility: visible;
+        opacity: 1;
+        margin-top: 0;
+        `;
+    };
+    document.querySelector('.close').onclick = function popUpClose() {
+        close = document.querySelector('.my-popup');
+        // slise.style.cssText = `
+        // margin: -650px;
+        // `
+        close.style.cssText =`
+        visibility: hidden;
+        opacity: 0;
+        margin-top: -650px;
+        `;
+    };
+
+};
+
+
+
+// popUpWindow();
+
