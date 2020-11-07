9a7fa106eb0c52cf82a2e6886a9cb7570b92cf17
Igor <learning@gmail.com>
2020-11-07 20:55:47 +0300

    Scroll bar fix

--- js/myscript.js
+++ js/myscript.js
@@ -12,7 +12,6 @@ setInterval(function puffOut() {
   },1000);
 },5000);
 
-
 window.onload = function () {
           ///////////////////////////////////////////////////////////////////////////////////
           
@@ -23,30 +22,60 @@ window.onload = function () {
         slise = document.querySelector('.my-popup'),
         closest = document.querySelectorAll(".closed"),
         openPopUp = document.querySelectorAll('.open-popup'),
-        popUpWrapper = document.querySelector('.my-popup-wrapper');
+        popUpWrapper = document.querySelector('.my-popup-wrapper'),
+        scrollHidden = document.querySelector('body'),
+        timeout = 500;
+const lockPaddingValue = window.innerWidth - popUpWrapper.offsetWidth + 'px';
 
       openPopUp.forEach(function (elem) {
         elem.addEventListener('click', function () {
             slise.classList.add('my-popup-visible'); 
             outPopUp.classList.add('my-popup-style-visible');
+
             
-            if (slise.classList.contains('my-popup-visible')){
-                document.addEventListener('click', function (event) {
-                  if(event.target === popUpWrapper){
-                    slise.classList.remove('my-popup-visible'); 
-                    outPopUp.classList.remove('my-popup-style-visible');
-                  }
-                });
-
-            }
-
+            scrollHidden.style.cssText = `
+            overflow-y: hidden;
+            padding-right: ${lockPaddingValue};
+            `;
+            closeOut();
           });
       });  
-        
+ 
+      function closeOut() {
+        if (slise.classList.contains('my-popup-visible')){
+          popUpWrapper.addEventListener('click', function (event) {
+            if(event.target === popUpWrapper){
+                      slise.classList.remove('my-popup-visible'); 
+                      outPopUp.classList.remove('my-popup-style-visible');
+                      setTimeout(function () {
+                        scrollHidden.classList.remove('lock-body');
+                        scrollHidden.style.paddingRight = '';
+                      }, timeout);
+
+                    }
+          });
+        }
+      }
+////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////
       closest.forEach(function(elem) { 
-        elem.addEventListener("click", function(){ 
+        elem.addEventListener("click", function(e){ 
           slise.classList.remove('my-popup-visible'); 
           outPopUp.classList.remove('my-popup-style-visible');
+          // scrollHidden.classList.remove('lock-body');
+          // scrollHidden.style.paddingRight = '';
+
+          setTimeout(function () {
+            scrollHidden.style.cssText = `
+            overflow-y: '';
+            padding-right: '';
+            `;
+          }, timeout);
+
+          e.preventDefault();
         });
       });
 
