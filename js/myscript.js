3cc8f2e2c348a0d23da64b2950280940ebbacd8d
Igor <learning@gmail.com>
2020-11-07 15:57:02 +0300

    popup 2.0

--- js/myscript.js
+++ js/myscript.js
@@ -1,27 +1,19 @@
 const btnRubberBand = document.querySelector('.my-button');
 const animista = document.querySelector('.puff');
 
-
 setInterval(function start() {
-    
     btnRubberBand.classList.toggle('animate__rubberBand');
- 
 },2500);
 
 setInterval(function puffOut() {
-
   animista.classList.add('puffOut');
-setTimeout(function puffOutRemove() {
-  animista.classList.remove('puffOut');
-},1000);
-  
+  setTimeout(function puffOutRemove() {
+    animista.classList.remove('puffOut');
+  },1000);
 },5000);
 
 
 window.onload = function () {
-
-        
-
           ///////////////////////////////////////////////////////////////////////////////////
           
           
@@ -30,23 +22,34 @@ window.onload = function () {
     let outPopUp = document.querySelector('.my-popup-style'),
         slise = document.querySelector('.my-popup'),
         closest = document.querySelectorAll(".closed"),
-        openPopUp = document.querySelectorAll('.open-popup');          
+        openPopUp = document.querySelectorAll('.open-popup'),
+        popUpWrapper = document.querySelector('.my-popup-wrapper');
 
       openPopUp.forEach(function (elem) {
         elem.addEventListener('click', function () {
-            // slise.classList.add('my-popup-visible', outPopUp.classList.add('my-popup-style-visible'));
             slise.classList.add('my-popup-visible'); 
             outPopUp.classList.add('my-popup-style-visible');
+            
+            if (slise.classList.contains('my-popup-visible')){
+                document.addEventListener('click', function (event) {
+                  if(event.target === popUpWrapper){
+                    slise.classList.remove('my-popup-visible'); 
+                    outPopUp.classList.remove('my-popup-style-visible');
+                  }
+                });
+
+            }
+
           });
       });  
-
         
-      closest.forEach(function(elem) { // проходим все элементы коллекции
+      closest.forEach(function(elem) { 
         elem.addEventListener("click", function(){ 
           slise.classList.remove('my-popup-visible'); 
           outPopUp.classList.remove('my-popup-style-visible');
         });
       });
+
 };
 
 
